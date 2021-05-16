a011ad60dc92f855af542baf9741254012b0644a
ghassen <ghassen.khalouaoui15@gmail.com>
2021-05-16 22:57:24 +0100

    authentication

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styleUrls: ['./bs-navbar.component.css']
+})
+export class BsNavbarComponent  {
+  user: firebase.User;
+
+  constructor(private afAuth: AngularFireAuth) { 
+    afAuth.authState.subscribe(user => this.user = user);
+  }
+
+  logout(){
+    this.afAuth.signOut();
+  }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent  {
+
+  constructor(private afAuth: AngularFireAuth) {
+  }
+
+  login(){
+    this.afAuth.signInWithRedirect( new firebase.auth.GoogleAuthProvider());
+  }
+}
