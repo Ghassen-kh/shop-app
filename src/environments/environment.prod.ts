22d425124497c961197de37135624ca9e0c615ec
ghassen <ghassen.khalouaoui15@gmail.com>
2021-05-16 17:47:49 +0100

    First Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: { 
+    apiKey: "AIzaSyCTu25CxZi0F-OhTjauy_6XDJCaBvXPHuU",
+    authDomain: "shop-app-29324.firebaseapp.com",
+    projectId: "shop-app-29324",
+    storageBucket: "shop-app-29324.appspot.com",
+    messagingSenderId: "639825104375",
+    appId: "1:639825104375:web:e6b9faa06f437fd28fb2f2",
+    measurementId: "G-GP2MXRGQRW"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: { 
+    apiKey: "AIzaSyCTu25CxZi0F-OhTjauy_6XDJCaBvXPHuU",
+    authDomain: "shop-app-29324.firebaseapp.com",
+    projectId: "shop-app-29324",
+    storageBucket: "shop-app-29324.appspot.com",
+    messagingSenderId: "639825104375",
+    appId: "1:639825104375:web:e6b9faa06f437fd28fb2f2",
+    measurementId: "G-GP2MXRGQRW"
+  }
 };
 
 /*
